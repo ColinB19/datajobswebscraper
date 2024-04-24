338e318bac9053fdbfb00c23294e813ca24d6516
ColinB19 <c.bradley.do@gmail.com>
2024-04-24 01:01:11 -0700

    added to the how to notebook

--- JobScraper.py
+++ JobScraper.py
@@ -1,6 +1,5 @@
 from driver_builder import DriverBuilder
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
